a3364ccff744662e1791fbe55c3e759a8898e949
Vincent Sengel <vincent.sengel@gmail.com>
2022-03-24 17:32:46 +0100

    creates routes controller validations and relations for booking

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
+  belongs_to :hotel
+  has_many :bookings
+
   validates :price_per_night, presence: true, numericality: { greater_than: 0 }
   validates :capacity, presence: true, numericality: { greater_than: 0 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 
 Rails.application.routes.draw do
+  get 'bookings/create'
   get 'rooms/index'
   get 'rooms/show'
   devise_for :users
   root to: 'pages#home'
   resources :rooms, only: [ :index, :show ]
+  resources :bookings, only: [ :create ]
 end

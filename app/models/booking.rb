13f831348f84ce52cd39f5133214deee821e6118
Vincent Sengel <vincent.sengel@gmail.com>
2022-03-25 00:30:45 +0100

    flatpickr working

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,15 @@ class Booking < ApplicationRecord
 
   validates :starts_at, presence: true
   validates :ends_at, presence: true
+  validate :ends_at_after_starts_at
+
+  private
+
+  def ends_at_after_starts_at
+    return if ends_at.blank? || starts_at.blank?
+
+    if ends_at < starts_at
+      errors.add(:ends_at, "must be after the start date")
+    end
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,13 @@
 class Room < ApplicationRecord
   belongs_to :hotel
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :price_per_night, presence: true, numericality: { greater_than: 0 }
   validates :capacity, presence: true, numericality: { greater_than: 0 }
 
+  def unavailable_dates
+    bookings.pluck(:starts_at, :ends_at).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end

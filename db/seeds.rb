d8866269302f7bfc969d700bae18f9af90774a04
Vincent Sengel <vincent.sengel@gmail.com>
2022-03-24 20:14:23 +0100

    creates basic room index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hotel_name = []
+
+20.times do
+  hotel_name << "#{Faker::Creature::Animal.name.capitalize} Hotel"
+end
+
+hotel_name.uniq.each do |hotel|
+  Hotel.new(address: Faker::Address::full_address, name: hotel).save
+  puts "#{Hotel.last.name} created"
+end
+
+130.times do
+  Room.new(price_per_night: (rand(39..189) * 10), capacity: rand(2..8), hotel_id: Hotel.all.sample().id).save
+  puts "new room created in #{Room.last.hotel.name}"
+end
